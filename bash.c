196cc7ac974f1cfaa0c947e80b560eca34be2e98
RafalWilinski <tho.ornee@gmail.com>
2015-12-24 22:00:39 +0100

    Cutting only flag arguments into separate array, cleanup

--- bash.c
+++ bash.c
@@ -8,23 +8,18 @@
 
 char **args;
 
+// @TODOS
+// -Override ctrl+c signal to stop child task - executor
+// -Fix output to file
+// -Add input from file 
+// -Add support for double >> 
+
 int get_line_length(char *line) {
 	int i = 0;
 	while(line[i]) {
 		i++;
 	}
 	return i;
-}
-
-char* isolate_command(char* line) {
-	unsigned int i = 0;
-	while(line[i] && line[i] != ' ') {
-		i++;
-	}
-	char* command = calloc(sizeof(char), i+1);
-	memcpy(command, line, i);
-	command[i] = '\0';
-	return command;
 }
 
 char** isolate_arguments(char* line) {
@@ -47,59 +42,84 @@ char** isolate_arguments(char* line) {
 	return args;
 }
 
-int execute_line(char *line) {
-	char* command = isolate_command(line);
-	printf("Command isolated: %s\n", command);
+char** isolate_command_arguments(char** arguments) {
+	int i = 0;
+	int arguments_count = 1;
+	int iter = 1;
 
-	char** args = isolate_arguments(line);
-	printf("Isolated arguments: \n");
-
-	int argc = 0;
-
-	int i = 1;
-	while(args[i]) {
-		printf("Arg[%d]: %s\n", i, args[i]);
+	// Find how many arguments are there, +1 every - sign
+	while(arguments[i]) {
+		if(arguments[i][0] == '-') {
+			arguments_count++;
+		}
 		i++;
 	}
 
-	if(strcmp(command, "exit") == 0) exit(0);
 
-	// Copy reference to STDIN and STDOUT
+	char **cmd_args = (char**) calloc(sizeof(char*), arguments_count);
+	cmd_args[0] = arguments[0];
+	i = 1;
+
+	// Until there are more arguments
+	while(arguments[i]) {
+
+		// If string begins with - (or --) it's flag
+		if(arguments[i][0] == '-') {
+
+			//Put it into array as iter-th argument
+			cmd_args[iter] = arguments[i];
+			iter++;
+		}
+
+		i++;
+	}
+
+	return cmd_args;
+}
+
+int execute_line(char *line) {
+	int file_descriptor;
+	char** args = isolate_arguments(line);
+	int argc = 0;
+
+	if(strcmp(args[0], "exit") == 0) exit(0);
+
+	if(fork() == 0) {
+
+		// Copy reference to STDIN and STDOUT
 		int stdin_copy = dup(0);
 		int stdout_copy = dup(1);
-		int file_descriptor;
+
+		char **command_args = isolate_command_arguments(args);
 
 		while(args[argc]) {
-			if(args[argc][0] == '>') { // If this sign was detected, print output to file (next arg)
+			if(strcmp(args[argc], ">") == 0) { // Print output to file
 				if(args[argc+1]) {
 					close(1);
 					file_descriptor = open(args[argc+1], O_WRONLY | O_CREAT, 0644);
+				}
+			} else if (strcmp(args[argc], ">>") == 0)  { // Append output to file
+				if(args[argc+1]) {
+					close(1);
+					file_descriptor = open(args[argc+1], O_WRONLY | O_APPEND, 0644);
 				}
 			}
 			argc++;
 		}
 
-
-	if(fork() == 0) {
-
-		printf("Execution... \n");
-		execvp(command, args);
-
+		execvp(command_args[0], command_args);
 		close(file_descriptor);
-		dup2(stdout_copy, 1);
-		close(stdout_copy);
-
+		perror("execvp");
 		exit(0);
 	}
-
 	wait(NULL);
+	printf("Execute command end.\n");
 	return 0;
 }
 
 int main() {
 	char* line;
 	while((line=readline("sh > ")) != NULL) {
-		printf("%s\n", line);
 		if(*line) {
 			add_history(line);
 			execute_line(line);
